379c08ba1964377aa3f2a4ebe7fe10caa61d3ff6
Doug Lokrantz <doug.lokrantz@gmail.com>
2018-05-29 18:21:02 +0000

    automatically backed up by learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -7,11 +7,13 @@ class EmailParser
   
   attr_accessor :emails
   
-
+  def initialize(emails)
+    @emails = emails
+  end
   
   
   def parse
-    emails.split(/[,|\s]/)-[""]
+    @emails = emails.split(/[,|\s]/)-[""]
   end 
 end
   
